ec50141aafa4a92b2786a677339a465917fe74f6
Ceruin <13532471+Ceruin@users.noreply.github.com>
2023-08-10 11:20:53 -0400

    .

--- Monster Guardian 2/Assets/Scripts/Player/PlayerCamera.cs
+++ Monster Guardian 2/Assets/Scripts/Player/PlayerCamera.cs
@@ -24,6 +24,8 @@ public class PlayerCamera : MonoBehaviour
         actions.Player.CameraRotate.canceled += CameraRotate_canceled;
         actions.Enable();
 
+        Cursor.lockState = CursorLockMode.None;
+
         originalOffset = transform.position - target.position;
 
         distance = originalOffset.magnitude;
@@ -86,40 +88,7 @@ public class PlayerCamera : MonoBehaviour
         _cameraInput = Vector2.zero;
     }
 }
-//using UnityEngine;
-//using UnityEngine.InputSystem;
 
-//public class PlayerCamera : MonoBehaviour
-//{
-//    private Vector2 _cameraInput;
-//    private float yaw = 0.0f;
-//    private float pitch = 0.0f;
-//    public Transform target;
-//    public Vector2 sensitivity = new Vector2(0.01f, 0.01f);
-//    public float distance = 10.0f;
-//    private PlayerControls actions;
-//    private Vector3 originalOffset;
-//    private int verticalPitch = 85;
-
-//    private void Awake()
-//    {
-//        actions = new PlayerControls();
-
-//        actions.Player.RightClick.performed += RightClick_performed;
-//        actions.Player.RightClick.canceled += RightClick_canceled;
-//        actions.Player.CameraRotate.performed += CameraRotate_performed;
-//        actions.Player.CameraRotate.canceled += CameraRotate_canceled;
-
-//        actions.Enable();
-
-//        Cursor.lockState = CursorLockMode.None;
-
-//        originalOffset = transform.position - target.position;
-
-//        distance = originalOffset.magnitude;
-//        yaw = Mathf.Atan2(originalOffset.x, originalOffset.z);
-//        pitch = Mathf.Asin(originalOffset.y / distance);
-//    }
 
 //    private void Update()
 //    {
@@ -151,23 +120,3 @@ public class PlayerCamera : MonoBehaviour
 //        }
 //    }
 
-//    private void RightClick_canceled(InputAction.CallbackContext obj)
-//    {
-//        _cameraInput = Vector2.zero;
-//        originalOffset = transform.position - target.position;
-//    }
-
-//    private void RightClick_performed(InputAction.CallbackContext obj)
-//    {
-//    }
-
-//    private void CameraRotate_canceled(InputAction.CallbackContext context)
-//    {
-//        _cameraInput = Vector2.zero;
-//    }
-
-//    private void CameraRotate_performed(InputAction.CallbackContext context)
-//    {
-//        _cameraInput = context.ReadValue<Vector2>();
-//    }
-//}
--- Monster Guardian 2/Assets/Scripts/Player/PlayerMovement.cs
+++ Monster Guardian 2/Assets/Scripts/Player/PlayerMovement.cs
@@ -6,27 +6,49 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public float rotationSpeed = 720.0f;
     public float jumpForce = 5f;
-    private CharacterController controller;
+    private RigidBody body;
     private Vector2 _moveInput;
     private float verticalVelocity = 0.0f;
     private bool jumpRequest = false;
+    private PlayerControls actions;
 
     private void Awake()
     {
-        controller = GetComponent<CharacterController>();
+        body = GetComponent<RigidBody>();
+        actions = new PlayerControls();
+
+        actions.Player.Movement.performed += OnMove_performed;
+        actions.Player.Movement.canceled += OnMove_canceled;
+
+        actions.Player.Jump.performed += OnJump_performed;
     }
 
-    public void OnMove(InputAction.CallbackContext context)
+        private void OnEnable()
+    {
+        // Enable the input actions when the object is enabled
+        actions.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // Disable the input actions when the object is disabled
+        actions.Player.Disable();
+    }
+
+    public void OnMove_performed(InputAction.CallbackContext context)
     {
         _moveInput = context.ReadValue<Vector2>();
     }
 
-    public void OnJump(InputAction.CallbackContext context)
+    public void OnMove_canceled(InputAction.CallbackContext context)
     {
-        if (controller.isGrounded && context.started)
-        {
-            jumpRequest = true;
-        }
+        _moveInput = Vector2.zero;
+    }
+
+    public void OnJump_performed(InputAction.CallbackContext context)
+    {
+        // Set a flag to indicate that a jump has been requested
+        jumpRequest = true;
     }
 
     private void Update()
@@ -36,122 +58,18 @@ public class PlayerMovement : MonoBehaviour
 
     private void HandleMovement()
     {
+        // Convert the movement input into the camera's coordinate space
         Vector3 cameraForward = Camera.main.transform.forward;
         Vector3 cameraRight = Camera.main.transform.right;
+
+        // Ignore the y component of the camera's forward vector
         cameraForward.y = 0;
+
+        // Normalize the vectors (important if the camera isn't level)
         cameraForward.Normalize();
         cameraRight.Normalize();
 
         Vector3 movement = (cameraForward * _moveInput.y + cameraRight * _moveInput.x) * moveSpeed * Time.deltaTime;
-
-        if (controller.isGrounded)
-        {
-            verticalVelocity = 0;
-
-            if (jumpRequest)
-            {
-                verticalVelocity = jumpForce;
-                jumpRequest = false;
-            }
-        }
-        else
-        {
-            verticalVelocity += Physics.gravity.y * Time.deltaTime;
-        }
-
-        Vector3 finalMovement = movement + new Vector3(0, verticalVelocity, 0) * Time.deltaTime;
-
-        if (movement != Vector3.zero)
-        {
-            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }
-        controller.Move(finalMovement);
-    }
-}
-
-
-//using Cinemachine;
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
-//using UnityEngine.InputSystem;
-
-//public class PlayerMovement : MonoBehaviour
-//{
-//    public float moveSpeed = 5f;
-//    public float rotationSpeed = 720.0f; // New variable to control the rotation speed
-//    private CharacterController controller;
-//    private bool jumpRequest = false; // Variable to track if a jump has been requested
-//    public float jumpForce = 5f;  // New variable to control the jumping power
-//    private Vector2 _moveInput;
-//    private CinemachineFreeLook _camera;
-
-//    private PlayerControls actions;
-//    private float verticalVelocity = 0.0f;
-
-//    private void Awake()
-//    {
-//        controller = GetComponent<CharacterController>();
-//        actions = new PlayerControls();
-
-//        actions.Player.Movement.performed += OnMove_performed;
-//        actions.Player.Movement.canceled += OnMove_canceled;
-
-//        actions.Player.Jump.performed += OnJump_performed;
-//    }
-
-//    private void OnEnable()
-//    {
-//        // Enable the input actions when the object is enabled
-//        actions.Player.Enable();
-//    }
-
-//    private void OnDisable()
-//    {
-//        // Disable the input actions when the object is disabled
-//        actions.Player.Disable();
-//    }
-
-//    private void Update()
-//    {
-//        HandleMovement();
-//    }
-
-//    private void FixedUpdate()
-//    {
-//    }
-
-//    public void OnMove_performed(InputAction.CallbackContext context)
-//    {
-//        _moveInput = context.ReadValue<Vector2>();
-//    }
-
-//    public void OnMove_canceled(InputAction.CallbackContext context)
-//    {
-//        _moveInput = Vector2.zero;
-//    }
-
-//    public void OnJump_performed(InputAction.CallbackContext context)
-//    {
-//        // Set a flag to indicate that a jump has been requested
-//        jumpRequest = true;
-//    }
-
-//    private void HandleMovement()
-//    {
-//        // Convert the movement input into the camera's coordinate space
-//        Vector3 cameraForward = Camera.main.transform.forward;
-//        Vector3 cameraRight = Camera.main.transform.right;
-
-//        // Ignore the y component of the camera's forward vector
-//        cameraForward.y = 0;
-
-//        // Normalize the vectors (important if the camera isn't level)
-//        cameraForward.Normalize();
-//        cameraRight.Normalize();
-
-//        Vector3 movement = (cameraForward * _moveInput.y + cameraRight * _moveInput.x) * moveSpeed * Time.deltaTime;
 
 //        // Apply gravity
 //        if (controller.isGrounded)
@@ -170,24 +88,24 @@ public class PlayerMovement : MonoBehaviour
 //            verticalVelocity += Physics.gravity.y * Time.deltaTime;  // Apply gravity
 //        }
 
-//        // Combine the horizontal and vertical movement
-//        Vector3 finalMovement = movement + new Vector3(0, verticalVelocity, 0) * Time.deltaTime;
+        // Combine the horizontal and vertical movement
+        Vector3 finalMovement = movement + new Vector3(0, verticalVelocity, 0) * Time.deltaTime;
 
-//        // If there's some horizontal movement, rotate the character to face the move direction
-//        if (movement != Vector3.zero)
-//        {
-//            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
-//            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-//        }
-//        controller.Move(finalMovement);
-//    }
+        // If there's some horizontal movement, rotate the character to face the move direction
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+        controller.Move(finalMovement);
+    }
 
-//    //private void HandleRotation(Vector3 moveDir)
-//    //{
-//    //    if (moveDir != Vector3.zero) // only change rotation when moving
-//    //    {
-//    //        Quaternion toRotation = Quaternion.LookRotation(moveDir, Vector3.up);
-//    //        transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.fixedDeltaTime);
-//    //    }
-//    //}
-//}
+    private void HandleRotation(Vector3 moveDir)
+    {
+        if (moveDir != Vector3.zero) // only change rotation when moving
+        {
+            Quaternion toRotation = Quaternion.LookRotation(moveDir, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.fixedDeltaTime);
+        }
+    }
+}
