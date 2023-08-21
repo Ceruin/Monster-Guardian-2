808378362f1be101e2b93bb22ffb1c39bb1e36ee
Ceruin <13532471+Ceruin@users.noreply.github.com>
2023-08-21 00:30:11 -0400

    .

--- Monster Guardian 2/Assets/Scripts/Mins/CubeminMovement.cs
+++ Monster Guardian 2/Assets/Scripts/Mins/CubeminMovement.cs
@@ -8,6 +8,11 @@ public class CubeminMovement : MonoBehaviour
     private bool isFollowingPlayer = false;
     private Rigidbody rb;
 
+    public Transform target;
+    public float speed = 5f;
+    public float stoppingDistance = 1f;
+    private Vector3 desiredVelocity;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -25,6 +30,25 @@ public class CubeminMovement : MonoBehaviour
     {
         Vector3 direction = (playerTransform.position - transform.position).normalized;
         rb.MovePosition(transform.position + direction * followSpeed * Time.fixedDeltaTime);
+    }
+
+    private void MoveTowardsTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        desiredVelocity = direction * speed;
+
+        // Calculate the force needed to reach the desired velocity
+        Vector3 force = desiredVelocity - rb.velocity;
+        force.y = 0; // Assuming you want to keep the AI character grounded
+
+        // Apply the force to the Rigidbody
+        rb.AddForce(force, ForceMode.Acceleration);
+
+        //// Stop the AI character when it's close to the target
+        //if (Vector3.Distance(transform.position, target.position) & lt; stoppingDistance)
+        //{
+        //    rb.velocity = new Vector3(0, rb.velocity.y, 0);
+        //}
     }
 
     public void SetFollowPlayer(bool follow)
--- Monster Guardian 2/Assets/Scripts/Player/PlayerMovement.cs
+++ Monster Guardian 2/Assets/Scripts/Player/PlayerMovement.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
+    public float moveSpeed = 500f;
     public float rotationSpeed = 720.0f;
     public float jumpForce = 5f;
     private Rigidbody body;
@@ -23,7 +23,7 @@ public class PlayerMovement : MonoBehaviour
         actions.Player.Jump.performed += OnJump_performed;
     }
 
-        private void OnEnable()
+    private void OnEnable()
     {
         // Enable the input actions when the object is enabled
         actions.Player.Enable();
@@ -71,23 +71,6 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 movement = (cameraForward * _moveInput.y + cameraRight * _moveInput.x) * moveSpeed * Time.deltaTime;
 
-//        // Apply gravity
-//        if (controller.isGrounded)
-//        {
-//            verticalVelocity = 0;  // Reset the vertical velocity if the character is grounded
-
-//            // If a jump has been requested, apply an upward force
-//            if (jumpRequest)
-//            {
-//                verticalVelocity = jumpForce;
-//                jumpRequest = false;  // Reset the jump request
-//            }
-//        }
-//        else
-//        {
-//            verticalVelocity += Physics.gravity.y * Time.deltaTime;  // Apply gravity
-//        }
-
         // Combine the horizontal and vertical movement
         Vector3 finalMovement = movement + new Vector3(0, verticalVelocity, 0) * Time.deltaTime;
 
@@ -97,7 +80,7 @@ public class PlayerMovement : MonoBehaviour
             Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
-        //controller.Move(finalMovement);
+        body.velocity = finalMovement;
     }
 
     private void HandleRotation(Vector3 moveDir)
@@ -107,5 +90,25 @@ public class PlayerMovement : MonoBehaviour
             Quaternion toRotation = Quaternion.LookRotation(moveDir, Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.fixedDeltaTime);
         }
+    }
+
+    private void HandleJump()
+    {
+        //        // Apply gravity
+        //        if (controller.isGrounded)
+        //        {
+        //            verticalVelocity = 0;  // Reset the vertical velocity if the character is grounded
+
+        //            // If a jump has been requested, apply an upward force
+        //            if (jumpRequest)
+        //            {
+        //                verticalVelocity = jumpForce;
+        //                jumpRequest = false;  // Reset the jump request
+        //            }
+        //        }
+        //        else
+        //        {
+        //            verticalVelocity += Physics.gravity.y * Time.deltaTime;  // Apply gravity
+        //        }
     }
 }
